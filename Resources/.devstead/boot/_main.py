47059f576e81bd7642547992f441b959c3a06925
Kumar Gaurav <kgaurav@Kumars-MBP.Dlink>
2019-09-05 17:04:44 +0530

    ptach install script now ignores .gitkeep file

--- Resources/.devstead/boot/_main.py
+++ Resources/.devstead/boot/_main.py
@@ -24,15 +24,16 @@ patch_list = (os.listdir(patch_dir))
 patch_list.sort()
 
 for patch in patch_list:
-    if os.path.exists(f'{logs_dir}/patch_{patch}.log'):
-        # Patch install log found, must have been applied earlier
-        print(f'Patch {patch} is already applied, skipping...')
-    else:
-        # Apply the patch
-        patch_cmd = f'{patch_dir}/{patch}'
-        os.chmod(patch_cmd, os.stat(patch_cmd).st_mode | stat.S_IEXEC) # existing permission+= OWNER_CAN_EXECUTE
-        call(f'{patch_cmd} >> {logs_dir}/patch_{patch}.log 2>&1'.split(), cwd=patch_dir)
-        print(f'Patch {patch} applied.')
+    if patch.endswith('.py') or patch.endswith('.sh') :
+        if os.path.exists(f'{logs_dir}/patch_{patch}.log'):
+            # Patch install log found, must have been applied earlier
+            print(f'Patch {patch} is already applied, skipping...')
+        else:
+            # Apply the patch
+            patch_cmd = f'{patch_dir}/{patch}'
+            os.chmod(patch_cmd, os.stat(patch_cmd).st_mode | stat.S_IEXEC) # existing permission+= OWNER_CAN_EXECUTE
+            call(f'{patch_cmd} >> {logs_dir}/patch_{patch}.log 2>&1'.split(), cwd=patch_dir)
+            print(f'Patch {patch} applied.')
 
 #---------------------------- III/III --------------------------
 print('-- Running boot scripts')
