6dbbdc825be5f16d71881937f1474f2bc64dcaac
Kumar Gaurav <kgaurav@Kumars-MBP.Dlink>
2019-09-07 20:07:51 +0530

    avoid calling main.py, read vc details frm env vars

--- Resources/.devstead/boot/_main.py
+++ Resources/.devstead/boot/_main.py
@@ -1,12 +1,13 @@
 #! /usr/bin/python3
 
 from subprocess import call
+from subprocess import Popen
 import sys
 import os
 import stat
 
-vc_user     = sys.argv[1]                       # version control user name
-vc_pass     = sys.argv[2]                       # password for the version control user
+vc_user     = os.environ['DEVSTEAD_VC_USER']    # read version control user name from env vars
+vc_pass     = os.environ['DEVSTEAD_VC_PASS']    # read password for the version control user from env vars
 boot_dir    = '/home/vagrant/.devstead/boot'    # what happens at vm boot/startup?
 patch_dir   = '/home/vagrant/.devstead/patches' # patches are minor update to the vm, applies on next boot
 logs_dir    = '/home/vagrant/.devstead/logs'    # traces i)each reboot and ii)every now and then patching of the vm
@@ -30,9 +31,10 @@ for patch in patch_list:
             print(f'Patch {patch} is already applied, skipping...')
         else:
             # Apply the patch
-            patch_cmd = f'{patch_dir}/{patch}'
+            patch_cmd   = f'{patch_dir}/{patch}'
+            log_file    = open(f'{logs_dir}/patch_{patch}.log', "w")
             call(f'chmod +x {patch_cmd}'.split(), cwd=patch_dir)
-            call(f'{patch_cmd} >> {logs_dir}/patch_{patch}.log 2>&1'.split(), cwd=patch_dir)
+            call(patch_cmd.split(), stdout=log_file, stderr=log_file, cwd=patch_dir)
             print(f'Patch {patch} applied.')
 
 #---------------------------- III/III --------------------------
@@ -40,7 +42,7 @@ print('-- Running boot scripts')
 
 for boot_script in os.listdir(boot_dir):
     if boot_script.endswith('.py') or boot_script.endswith('.sh'):
-        # Run all bootscripts,  avoid calling current(this) script again
+        # Run all bootscripts,  avoid running the calling script again
         if boot_script == os.path.basename(__file__): continue 
         print('-- script name: ' + boot_script)
         call(f'{boot_dir}/{boot_script}'.split(), cwd=boot_dir)
